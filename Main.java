045fbf1ffee899bf979200a91aba9fc9d0639c42
Adams505 <101186913+Adams505@users.noreply.github.com>
2022-03-22 11:49:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("podaj liczbe a");
+    int a = scan.nextInt();
+    int b=a%10;
+    System.out.println(b);
+    
+    
+  }
+}
